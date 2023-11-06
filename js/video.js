1653566286bee534d8a6e4fc2b5827acd6127d5a
porygonisaduck <alvaradx@umich.edu>
2023-11-05 23:53:35 -0500

    Completed first task

--- js/video.js
+++ js/video.js
@@ -1,8 +1,8 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	video.autoplay = false;
+	video.loop = false;
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
