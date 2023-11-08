39647f891ec6013f502c918682b5b8531398d17a
porygonisaduck <alvaradx@umich.edu>
2023-11-07 20:19:44 -0500

    Completed all tasks

--- js/video.js
+++ js/video.js
@@ -5,7 +5,60 @@ window.addEventListener("load", function() {
 	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector("#volume").innerHTML = document.querySelector("#slider").value;
+
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.1;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if ((video.currentTime + 10) > video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 10;
+	}
+	console.log(video.currentTime);
+	console.log(video.duration);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		this.innerHTML = "Unmute";
+		video.muted = false;
+	}
+	else {
+		this.innerHTML = "Mute";
+		video.muted = true;
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = this.value;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
+
 
